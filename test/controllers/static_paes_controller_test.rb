00c1c184b5d9c195be19c28f37d4054c801ffe92
TBATYOF <toribatyof@gmail.com>
2019-08-07 13:25:07 +0000

    Add a Static Pages controller

---
+++ test/controllers/static_paes_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPaesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_paes_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get static_paes_help_url
+    assert_response :success
+  end
+
+end
